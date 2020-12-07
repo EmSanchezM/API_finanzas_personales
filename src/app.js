09a68986a83b75d47974d64dff7a30a24fc7b450
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-12-07 11:03:04 -0600

    routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const ingresoRoute = require('./routes/ingreso.routes');
+const egresoRoute = require('./routes/egreso.routes');
+const prestamoRoute = require('./routes/prestamo.routes');
+const ahorroRoute = require('./routes/ahorro.routes');
+
 const app = express();
 
 app.set("port", process.env.PORT || 5000);
@@ -11,5 +16,10 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/ingresos', ingresoRoute);
+app.use('/api/egresos',  egresoRoute);
+app.use('/api/prestamos', prestamoRoute);
+app.use('/api/ahorros', ahorroRoute);
 
 module.exports = app;
